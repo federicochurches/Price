d76555ff8e966555eacbdbf652c43569e9484c33
federicochurches <federico.churches@gmail.com>
2026-06-27 19:08:56 -0500

    fix: border-top RE/PA via JS col en lugar de CSS scoped (closest .section-cr era false)

--- demo_js_main.js
+++ demo_js_main.js
@@ -104,6 +104,8 @@ function w22_renderRE(open){
  W.reOpen=open;
  var d=data(), col=cv().col;
  var el=g('w22-re-list');if(!el)return;
+ el.style.borderTop='3px solid '+col;
+ var pg=g('w22-pg');if(pg)pg.style.borderTop='3px solid '+col;
  var items=d.re||[];
  el.innerHTML=items.map(function(r,i){
   var h=(i>=5&&!W.reOpen)?' style="display:none;"':'';
